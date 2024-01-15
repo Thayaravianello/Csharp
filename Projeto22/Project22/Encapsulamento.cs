505750e9139dd1a87e02da09a7d8e31eee4058aa
Thayara Vianello <thayaraferreira@alunos.unicesumar.edu.br>
2024-01-15 16:20:22 +0100

    Projeto22 encapsulamento

---
+++ Projeto22/Project22/Encapsulamento.cs
@@ -0,0 +1,84 @@
+﻿using System.Globalization;
+
+namespace Projeto15
+{
+
+    //------------------------------------------------------------------------------------------------------------------------------
+    //---------------ENCAPSULAMENTO------------------------------------------------------------------------------------------------
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        //------------------------------------------------------------------------------------------------------------------------------
+        //***************GET E SET -- GET PARA PEGAR A CLASSE NOME, SET PARA DAR NOVO NOME--------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------
+
+
+        public string GetNome() {  return _nome; }
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+
+
+        //------------------------------------------------------------------------------------------------------------------------------
+        //***************CONSTRUTOR PRINCIPAL---->CRIAR O CONSTRUTOR + MESMO NOME DA CLASSE () E SE VAI RECEBER PARAMETRO
+        //------------------------------------------------------------------------------------------------------------------------------
+
+        public Produto()
+        {
+
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------]
+
+        //FAZENDO SEGUNDO CONSTRUTOR
+
+        public Produto(string nome, double preco)
+        {
+            _nome = nome;
+            _preco = preco;
+            Quantidade = 0;// dispensavel pois ja inicia com o valor zero como explicado
+        }
+
+
+        //------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------
+        public double ValorTotalEmEstoque()
+        {
+            return _quantidade * _preco;
+        }
+        public void AdicionarProdutos(int quantidade)// VOID POIS NAO VAI RETORNAR VALOR MAS SIM FAZER CALCULO INTERNO
+        {
+            _quantidade = _quantidade + quantidade; // vai receber a QUANTIDADE maiscula com a quantidade minuscula 
+        }
+
+        public void RemoverProdutos(int quantidade)// VOID POIS NAO VAI RETORNAR VALOR MAS SIM FAZER CALCULO INTERNO
+        {
+            _quantidade = _quantidade - quantidade; // vai receber a QUANTIDADE maiscula com a quantidade minuscula 
+        }
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + " , "
+                + _quantidade + " Unidades , Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+            // CRINAOD O OVERRIDE STRING TOSTRING ESTAMOS CRIANDO UMA MENIRA MAIS FACIL DE CONCATENAR
+        }
+
+
+
+    }
+}
---
+++ Projeto22/Project22/Program.cs
@@ -0,0 +1,74 @@
+﻿
+/*using System.Globalization;
+
+namespace Projeto15
+{
+
+ //------------------------------------------------------------------------------------------------------------------------------
+ //------------------------------------------------------------------------------------------------------------------------------
+    class Produto
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+ //------------------------------------------------------------------------------------------------------------------------------
+//***************CONSTRUTOR PRINCIPAL---->CRIAR O CONSTRUTOR + MESMO NOME DA CLASSE () E SE VAI RECEBER PARAMETRO
+ //------------------------------------------------------------------------------------------------------------------------------
+
+        public Produto()
+        {
+
+        }
+      
+ //-----------------------------------------------------------------------------------------------------------------------------
+ //------------------------------------------------------------------------------------------------------------------------------
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+ //------------------------------------------------------------------------------------------------------------------------------
+ //------------------------------------------------------------------------------------------------------------------------------]
+
+        //FAZENDO SEGUNDO CONSTRUTOR
+
+        public Produto(string nomme, double preco)
+        {
+            Nome = nomme;
+            Preco = preco;
+            Quantidade = 0;// dispensavel pois ja inicia com o valor zero como explicado
+        }
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+        public void AdicionarProdutos(int quantidade)// VOID POIS NAO VAI RETORNAR VALOR MAS SIM FAZER CALCULO INTERNO
+        {
+            Quantidade = Quantidade + quantidade; // vai receber a QUANTIDADE maiscula com a quantidade minuscula 
+        }
+
+        public void RemoverProdutos(int quantidade)// VOID POIS NAO VAI RETORNAR VALOR MAS SIM FAZER CALCULO INTERNO
+        {
+            Quantidade = Quantidade - quantidade; // vai receber a QUANTIDADE maiscula com a quantidade minuscula 
+        }
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + " , "
+                + Quantidade + " Unidades , Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+            // CRINAOD O OVERRIDE STRING TOSTRING ESTAMOS CRIANDO UMA MENIRA MAIS FACIL DE CONCATENAR
+        }
+
+
+
+    }
+}
+*/
