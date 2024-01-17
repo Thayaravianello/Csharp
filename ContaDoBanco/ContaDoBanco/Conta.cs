3eb34f25af79d71c3d2853029c686a7256c90622
Thayara Vianello <thayaraferreira@alunos.unicesumar.edu.br>
2024-01-17 16:42:14 +0100

    Bank account

---
+++ ContaDoBanco/ContaDoBanco/Conta.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaDoBanco
+{
+
+
+    //--------------------------------------------------------------------------------------------------------
+    //-------------------------------------CLASSE CONTA BANCARIA-------------------------------------------
+    internal class ContaBanco
+    {
+
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+
+        //--------------------------------------------------------------------------------------------------------
+        //-------------------------------------CONSTRUCTOR 2 PARÂMETRO-------------------------------------------
+        public ContaBanco(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+
+        }
+
+
+        //--------------------------------------------------------------------------------------------------------
+        //-------------------------------------CONSTRUCTOR 3 PARÂMETRO-------------------------------------------
+        public ContaBanco(int numero, string titular, double saldo) : this(numero, titular)
+        {
+
+            Saldo = saldo;
+
+        }
+
+
+        //--------------------------------------------------------------------------------------------------------
+        //-------------------------------------TO STRING-------------------------------------------
+
+        public void Deposito(double quantia) // VOIDE NAO RETORNA VALOR SO ARMAZENA
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo-= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + " , Titular: " + Titular + ", Saldo: $:  " + Saldo.ToString("F2" , CultureInfo.InvariantCulture);
+
+
+            //--------FOI ESCRITO ENTRE "" OS NOMES QUE QUEREMOS IMPRESSOS---------------------
+            //-----PERCEBA QUE PARA MONSTAR O TEXTO DO CODIGO CHAMAMOS AS VARIAVEIS DA CLASSE-------
+
+        }
+    }
+}
+
+
---
+++ ContaDoBanco/ContaDoBanco/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaDoBanco
+{
+    class Program
+    {
+
+        //------DECLARAÇÃO DA CONTA BANCARIA CLASSE AQUI---------------------
+
+    
+        
+        static void Main(string[] args)
+        {
+
+            ContaBanco conta;
+            ContaBanco deposito;
+
+            Console.Write("Entre com o número da conta:");
+            int numero = int.Parse(Console.ReadLine());
+
+
+            Console.Write( "Entre com o nome do Titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá deposito inicial: s/n: ");
+            char resposta = char.Parse(Console.ReadLine());
+
+
+            if (resposta == 's' || resposta == 'S' )
+            {
+                Console.Write("Entre com o valor do deposito inicial : ");
+
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBanco(numero,titular,depositoInicial);
+
+            } else {
+                conta = new ContaBanco(numero, titular);
+
+               
+            }
+
+
+            //**************************DATE ACCOUNT******************************************************************
+            //--------------------------------------------------------------------------------------------------------
+
+            Console.WriteLine("");
+
+            Console.WriteLine("DADOS DA CONTA:");
+
+
+            Console.WriteLine(conta);
+
+            //**************************DATE ACCOUNT - DEPOSIT*********************************************************
+            //--------------------------------------------------------------------------------------------------------
+
+            Console.Write( "Entre com o valor para deposito: ");
+
+            double quantia = double.Parse(Console.ReadLine() , CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            
+            
+
+          
+            Console.WriteLine();
+            //**************************UPDATE ACCOUNT PLUS*****************************************************************
+            //--------------------------------------------------------------------------------------------------------
+
+            Console.WriteLine( "DADOS DA CONTA ATUALIZADOS: ");
+
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+
+            //**************************DATE ACCOUNT MINUS******************************************************************
+            //--------------------------------------------------------------------------------------------------------
+
+            Console.Write("Entre com os dados para saque: ");
+
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+
+
+
+
+            Console.WriteLine(  );
+
+            Console.WriteLine("DADOS DA CONTA ATUALIZADOS: ");
+
+            Console.WriteLine(conta);
+
+
+        }
+
+    }
+}
