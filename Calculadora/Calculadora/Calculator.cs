ff26404be448bf65ac1cf6e6f30cc274c510bfcf
Thayara Vianello <thayaraferreira@alunos.unicesumar.edu.br>
2024-01-27 18:48:20 +0100

    Mais  projetos

---
+++ Calculadora/Calculadora/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Calculos
+{
+    class Calculadora
+    {
+        public static int Sum(params int[] numbers)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+        
+
+
+        }
+
+    }
+
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Calculos
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            int s1 = Calculadora.Sum(2,5,8,6,3);
+
+            Console.WriteLine(s1);
+        }
+    }
+}
